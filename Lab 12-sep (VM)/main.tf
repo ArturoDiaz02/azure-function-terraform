34d6f00e40bf169c17467716d3aa7a7a70d61673
Carlos Arturo Diaz <diazartiagacarlosarturo@gmail.com>
2023-09-19 15:39:16 -0500

    Doc

--- Lab 12-sep (VM)/main.tf
+++ Lab 12-sep (VM)/main.tf
@@ -1,18 +1,22 @@
+# Definición del provider que ocuparemos
 provider "azurerm" {
   features {}
 }
 
+# Se crea el grupo de recursos, al cual se asociarán los demás recursos
 resource "azurerm_resource_group" "resource_group" {
   name     = var.name_function
   location = var.location
 }
 
+#Se crea el grupo de seguridad
 resource "azurerm_network_security_group" "network_security_group" {
   name                = "example-security-group"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
+#Se crea la regla de seguridad para el puerto 22
 resource "azurerm_network_security_rule" "ssh" {
   name                        = "AllowSSH"
   priority                    = 1001
@@ -27,6 +31,7 @@ resource "azurerm_network_security_rule" "ssh" {
   network_security_group_name = azurerm_network_security_group.network_security_group.name
 }
 
+#Se crea la red virtual
 resource "azurerm_virtual_network" "virtual_network" {
   name                = "example-network"
   location            = azurerm_resource_group.resource_group.location
@@ -38,6 +43,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   }
 }
 
+#Se crea el subred
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.resource_group.name
@@ -45,6 +51,7 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+#Se crea la ip publica
 resource "azurerm_public_ip" "public_ip" {
   name                = "example-public-ip"
   location            = azurerm_resource_group.resource_group.location
@@ -52,6 +59,7 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Dynamic"
 }
 
+#Se crea la interfaz de red
 resource "azurerm_network_interface" "network_interface" {
   name                = "example-nic"
   location            = azurerm_resource_group.resource_group.location
@@ -65,6 +73,7 @@ resource "azurerm_network_interface" "network_interface" {
   }
 }
 
+#Se crea la maquina virtual
 resource "azurerm_linux_virtual_machine" "VM" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.resource_group.name
@@ -75,16 +84,19 @@ resource "azurerm_linux_virtual_machine" "VM" {
     azurerm_network_interface.network_interface.id,
   ]
 
+  #Se crea la llave ssh
   admin_ssh_key {
     username   = var.username
     public_key = file("~/.ssh/id_rsa.pub")
   }
 
+  #Se crea el disco duro
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
+  #Se crea la imagen de la maquina virtual
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-focal"
